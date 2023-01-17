6e2f13dd0df736cab7c06f55aef080a7a5e2e370
Dmitry <dziamentsyeu.dzmitry@gmail.com>
2023-01-17 19:21:27 +0300

    add getTimer

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,17 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
+
     private Long nanoTime = System.nanoTime();
+
+    public Timer() {
+
+    }
 
     public Long getTime() {
         return nanoTime;
